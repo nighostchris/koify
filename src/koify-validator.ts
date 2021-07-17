c62b60bcf43a3cd906cfffe408fdae2ac61ce129
Chris <chrisliupascal@gmail.com>
2021-07-17 16:06:48 +0800

    Introduce various number type validators + Install Jest

--- src/koify-validator.ts
+++ src/koify-validator.ts
@@ -1,5 +1,17 @@
 export class KoifyValidator {
+  public static isInteger(input: any): boolean {
+    return KoifyValidator.isNumber(input) && !(input % 1);
+  }
+
+  public static isFloat(input: any): boolean {
+    return KoifyValidator.isNumber(input) && (input % 1 !== 0);
+  }
+
   public static isNumber(input: any): boolean {
     return typeof input === 'number' && !Number.isNaN(input) && Number.isFinite(input);
+  }
+
+  public static isString(input: any): boolean {
+    return typeof input === 'string';
   }
 }
